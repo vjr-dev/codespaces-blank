dfb7a1d8107cae6f07b797ca8cd6bb72c2c6e222
vjr-dev <109079505+vjr-dev@users.noreply.github.com>
2024-11-11 13:32:30 +0000

    setting up and running codespace

---
+++ DSA/searchingAlgo/bubbleSort.js
@@ -0,0 +1,16 @@
+/**
+ * Here is the bubble sort algorithm 
+ * 
+ * 1. create function
+ */
+
+
+
+
+function bubbleSort() {
+
+    console.log("I am coming from bubblesort file")
+}
+
+
+export default bubbleSort
---
+++ index.js
@@ -0,0 +1,4 @@
+import bubbleSort from "./DSA/searchingAlgo/bubbleSort.js"
+
+
+bubbleSort()
